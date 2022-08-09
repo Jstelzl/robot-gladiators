536ba721da647c8e57c7cc809e26704155212b05
[josiah stelzl] <[josiahstelzl@gmail.com]>
2022-08-09 10:22:57 -0400

    finished feature/random-order

--- assets/js/game.js
+++ assets/js/game.js
@@ -36,6 +36,11 @@ var fightOrSkip = function() {
 
 // fight function (now with parameter for enemy's name) 
 var fight = function(enemy) {
+  // keep track of who goes first
+  var isPlayerTurn = true;
+  if (Math.random() > 0.5) {
+    isPlayerTurn = false;
+  }
   while (playerInfo.health > 0 && enemy.health > 0) {
     // ask player if they'd like to fight or skip using fightOrSkip function 
     if (fightOrSkip()) {
